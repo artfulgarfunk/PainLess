e9a511c6824ffda557ed009708aa3c884198e700
Jack <92jackhenderson@gmail.com>
2017-04-17 21:28:20 +0100

    updated spec

--- spec/features/results_feature_spec.rb
+++ spec/features/results_feature_spec.rb
@@ -22,23 +22,6 @@ feature 'Trigger Foods' do
       click_link_or_button 'View Potential Trigger Foods'
       expect(page).to have_content 'hamburger'
     end
-
-    scenario "Display all foods that cause a pain score of 10" do
-      sign_up
-      log_painscore
-      log_foodlog('hamburger')
-      log_activity
-      log_painscore(10,"2017-03-17")
-      log_foodlog('wheat')
-      log_activity
-      log_painscore(10,"2017-03-17")
-      log_foodlog('beef')
-      log_activity
-      click_link_or_button 'View Potential Trigger Foods'
-      expect(page).not_to have_content('hamburger')
-      expect(page).to have_content('wheat')
-      expect(page).to have_content('beef')
-  end
 end
 
 end
